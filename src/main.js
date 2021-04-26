208be0376f849768edef5155c1b99f27599117a6
fakhri ahnaf dhia <fakhri_ahnaf01@yahoo.co.id>
2021-04-26 10:32:48 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import Home from './views/Home.vue';
+import Product from './views/Product.vue';
 
-Vue.config.productionTip = false
 
+// Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/product',
+    name: 'Product',
+    component: Product
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+];
+
+const router =new VueRouter({
+  routes
+});
 new Vue({
+  el: '#app',
+  router,
   render: h => h(App),
-}).$mount('#app')
+})
+
+
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="Home">
+   <!-- Header Section Begin -->
+    <Header/>
+    <BannerHome/>
+    <RecomendedSection/>
+    <InstagramSection/>
+    <Footer/>
+
+
+    </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+import BannerHome from '../components/BannerHome.vue';
+import RecomendedSection from '../components/RecomendedSection.vue';
+import InstagramSection from '../components/InstagramSection.vue';
+import Footer from '../components/Footer.vue';
+
+export default {
+    name: 'Home',
+    components: {
+        Header,
+        BannerHome,
+        RecomendedSection,
+        InstagramSection,
+        Footer,
+    }
+};
+</script>
